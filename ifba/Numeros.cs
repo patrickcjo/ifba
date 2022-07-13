e15f8e6557d69eab867b3ab7a40f404e83d39232
patrick carvalho <patrickcjo@hotmail.com>
2022-07-13 19:21:12 -0300

    13/07

---
+++ ifba/Numeros.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifba
+{
+
+    //*converter string em array de inteiros
+    
+    public class Numeros
+    {
+        public Numeros()
+        {
+
+        }
+
+        public static int Ehfeliz(int entrada)
+        {
+            string novaentrada = entrada.ToString();
+            var meuarray = novaentrada.Select(x => Int32.Parse(x.ToString())).ToList();
+            var resultado = Math.Pow(meuarray[0], 2) + Math.Pow(meuarray[1], 2) + Math.Pow(meuarray[2], 2);
+            return Convert.ToInt32(resultado);
+        }
+
+
+    }
+}
---
+++ ifbaxunit/UnitNumeros.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ifba;
+using Xunit;
+
+namespace ifbaxunit
+{
+    public class UnitNumeros
+    {
+        [Fact]
+
+        public void NFelizes130()
+        {
+            var resultado = Numeros.Ehfeliz(130);
+            Assert.True(resultado == 10);
+        }
+    }
+}
