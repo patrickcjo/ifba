0b9e2d46477c8cf87b4dd2cd196e1b19e09d2148
patrick carvalho <patrickcjo@hotmail.com>
2022-08-05 15:21:08 -0300

    a

---
+++ ifba/Soma.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifba
+{
+    public class Soma
+    {
+        public int numero { get; set; }
+
+        public Soma()
+        {
+
+        }
+
+        public int CalcularSoma(int resultado)
+        {
+            
+            for (int i = 0; i < numero; i++)
+            {
+                resultado = resultado + i;
+            
+            }
+            return resultado;
+        }
+    }
+}
---
+++ ifbaxunit/UnitSoma.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ifba;
+using Xunit;
+
+namespace ifbaxunit
+{
+    public class UnitSoma
+    {
+
+        [Fact]
+
+        public void CalcularSoma100()
+        {
+        
+            Soma  soma = new Soma();
+            soma.numero = 100;
+            int retorno  = soma.CalcularSoma(100);
+            Assert.True(retorno == 5050);
+
+        }
+
+        [Fact]
+
+        public void CalcularSoma10()
+        {
+
+            Soma soma = new Soma();
+            soma.numero = 10;
+            int retorno = soma.CalcularSoma(10);
+            Assert.True(retorno == 55);
+
+        }
+    }
+}
