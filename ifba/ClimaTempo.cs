580ac3bba30b6ee47df80da420a14a53d61d7c7d
patrick carvalho <patrickcjo@hotmail.com>
2022-08-10 09:45:21 -0300

    lista clima tempo

---
+++ ifba/ClimaTempo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifba
+{
+    public class ClimaTempo
+    {
+        public int temp { get; set; }
+        public int media { get; set; }
+
+        public ClimaTempo()
+        {
+
+        }
+
+        public int CalcularTemp()
+        {
+
+
+
+
+            return temp;
+        }
+    }
+}
