38b3b2f0844ecf5475729f143b79e7e1e82bfca9
patrick carvalho <patrickcjo@hotmail.com>
2022-08-13 21:24:23 -0300

    Clima temperatura

--- ifba/ClimaTempo.cs
+++ ifba/ClimaTempo.cs
@@ -8,21 +8,45 @@ namespace ifba
 {
     public class ClimaTempo
     {
-        public int temp { get; set; }
-        public int media { get; set; }
+        public int maiorTemp { get; set; }
+        public int menorTemp { get; set; }
+        public double media { get; set; }
 
         public ClimaTempo()
         {
 
         }
 
-        public int CalcularTemp()
+        public void CalcularTemp(List<int> minhalista)
         {
 
+            int soma = 0;
+            menorTemp = 1000;
+            maiorTemp = -500;
+            foreach (var item in minhalista)
+            {
+                soma += item;
+                if (item < menorTemp)
+                {
+                    menorTemp = item;
+                }
+                if (item > maiorTemp)
+                {
+                    maiorTemp = item;
+                }
+            }
+            media = soma / minhalista.Count;
 
-
-
-            return temp;
+        }
+        public static double CalcularMedia(List<int> minhalista)
+        {
+            int soma = 0;
+            foreach (var item in minhalista)
+            {
+                soma += item;
+            }
+            var newmedia = soma / minhalista.Count;
+            return newmedia;
         }
     }
 }
---
+++ ifbaxunit/UnitClimaTempo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ifba;
+using Xunit;
+
+namespace ifbaxunit
+{
+    public class UnitClimaTempo
+    {
+
+        [Fact]
+
+        public void CalcularMedia()
+        {
+            List<int> tempo = new List<int>() { 21, 20, 34, 12, 20, 21, 21, 27, 27, 35, 14, 29, 21, 25, 26, 21, 30, 27, 22, 28, 18, 13, 33, 13, 19, 19, 27, 17, 17, 23, 15, 22, 12, 20, 31, 22, 20, 34, 14, 26, 24, 28, 17, 23, 23, 23, 13, 18, 19, 18 };
+
+            ClimaTempo clima = new ClimaTempo();
+            clima.CalcularTemp(tempo);
+            Assert.True(clima.maiorTemp == 35);
+            Assert.True(clima.menorTemp == 12);
+            Assert.True(ClimaTempo.CalcularMedia(tempo) == 22.0);
+        }
+    }
+}
