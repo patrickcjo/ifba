1714e038c1fbb9a63d42cf7806f9fd095c5b220f
patrick carvalho <patrickcjo@hotmail.com>
2022-08-19 14:56:19 -0300

    PA

---
+++ ifba/PA.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifba
+{
+    public class PA
+    {
+
+        public int razao { get; set; }
+        public int qnt { get; set; }
+        public int n1 { get; set; }
+
+        public PA()
+        {
+
+        }
+
+        public void CalcularPA(List<int> minhalista)
+        {
+            for (int i = 0; i < qnt; i++)
+            {
+                razao=
+            }
+        }
+    }
+}
